a7aeb1253495afbd9b44eac40b03f700cd0c51b1
James Myose <jmyose@live.com>
2021-04-26 15:49:46 -0500

    Readding files

---
+++ Movies/MoviesData/IActorRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using MoviesData.Models;
+
+namespace MoviesData
+{
+    public interface IActorRepository
+    {
+        float ActorTotalSalary(string firstName, string lastName);
+    }
+}
---
+++ Movies/MoviesData/SqlReviewRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using MoviesData.DataDelegates.QuestionQueryDelegates;
+using MoviesData.DataDelegates.ReportQueryDelegates;
+using MoviesData.Models;
+using DataAccess;
+
+namespace MoviesData
+{
+    public class SqlReviewRepository: IReviewRepository
+    {
+        private readonly SqlCommandExecutor executor;
+        public SqlReviewRepository(string connectionString)
+        {
+            executor = new SqlCommandExecutor(connectionString);
+        }
+
+        public IReadOnlyList<Review> MovieReviews(string movieName)
+        {
+            var d = new MovieReviewsDataDelegate(movieName);
+            return executor.ExecuteReader(d);
+        }
+
+        public IReadOnlyList<Review> ScoreReviews(int score)
+        {
+            var d = new ScoreReviewsDataDelegate(score);
+            return executor.ExecuteReader(d);
+        }
+    }
+}
